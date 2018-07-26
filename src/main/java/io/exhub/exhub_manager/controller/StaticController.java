afbbccbd4c2679b89113e06a5e33c4a18af95dc4
livenQiang <qiangjun@aliyun.com>
2018-07-27 00:35:59 +0800

    update 后台模板

--- src/main/java/io/exhub/exhub_manager/controller/StaticController.java
+++ src/main/java/io/exhub/exhub_manager/controller/StaticController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.thymeleaf.TemplateEngine;
 
 /**
@@ -23,5 +24,24 @@ public class StaticController {
     public String getIndex() {
 
         return "login";
+    }
+
+    /**
+     * 进入后台管理运营平台首页
+     * @return
+     */
+    @GetMapping(value = "manager_index")
+    public String managerIndex(){
+        return "demo/index";
+    }
+
+    /**
+     * 进入页面
+     * @param pageName
+     * @return
+     */
+    @GetMapping(value = "showThisHtml.html")
+    public String managerListKyc(@RequestParam String pageName){
+        return "/demo/"+pageName;
     }
 }
